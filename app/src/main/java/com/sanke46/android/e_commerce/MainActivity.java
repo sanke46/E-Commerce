729a66a1f83d2640cd2f34293601cb852a96550d
devil.ilya@gmail.com <d@gamil.com>
2017-01-26 15:54:29 +0300

    + add one line in grable + MainActivity change extends ActionBarActivity + change style + add Toolbar

--- app/src/main/java/com/sanke46/android/e_commerce/MainActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/MainActivity.java
@@ -1,9 +1,10 @@
 package com.sanke46.android.e_commerce;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
