6d43ff131096b3e718317d9f63fe1802cd99fa97
devil.ilya@gmail.com <d@gamil.com>
2017-01-26 16:41:28 +0300

    change strings in java

--- app/src/main/java/com/sanke46/android/e_commerce/MainActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/MainActivity.java
@@ -29,14 +29,13 @@ public class MainActivity extends ActionBarActivity {
                 .withActionBarDrawerToggle(true)
                 .withHeader(R.layout.drawer_header)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
-                        new SectionDrawerItem().withName(R.string.drawer_item_settings),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                        new PrimaryDrawerItem().withName(R.string.piz_catalog).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.sales).withIcon(FontAwesome.Icon.faw_gamepad),
+                        new PrimaryDrawerItem().withName(R.string.my_order).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
+                        new SectionDrawerItem().withName(R.string.about_delevery),
+                        new SecondaryDrawerItem().withName(R.string.profile).withIcon(FontAwesome.Icon.faw_cog),
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
+                        new SecondaryDrawerItem().withName(R.string.sing_out).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
                 )
                 .build();
     }
