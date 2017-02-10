a462feb05e7ead13b12c548d0298a70f0983ba7a
Ilya fedoseev <shift.of.razum@gmail.com>
2017-02-10 22:49:55 +0300

    Add two Activities
    Profile + Sales

---
+++ app/src/main/java/com/sanke46/android/e_commerce/Sales.java
@@ -0,0 +1,13 @@
+package com.sanke46.android.e_commerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales);
+    }
+}
