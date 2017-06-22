8b9595c57d2cdf7d7e2d51df006b3400767682f8
sanke46 <shift.of.razum@gmail.com>
2017-06-22 17:05:53 +0100

    create User class

---
+++ app/src/main/java/com/sanke46/android/e_commerce/model/User.java
@@ -0,0 +1,53 @@
+package com.sanke46.android.e_commerce.model;
+
+/**
+ * Created by ilya on 22/06/2017.
+ */
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getter
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //setter
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
