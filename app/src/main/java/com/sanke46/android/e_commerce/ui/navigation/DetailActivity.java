07d3025bbf643e18d1a374968f9649692e5fc8be
Ilya Fedoseev <shift.of.razum@gmail.com>
2018-06-04 12:23:14 +0300

    Create Style "Forgot password" Done DetailActivity with style

--- app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/DetailActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/DetailActivity.java
@@ -1,24 +1,55 @@
 package com.sanke46.android.e_commerce.ui.navigation;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sanke46.android.e_commerce.R;
+import com.sanke46.android.e_commerce.adapter.ListInformationAdapter;
+import com.sanke46.android.e_commerce.model.InfoDetail;
 import com.sanke46.android.e_commerce.model.Item;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private BasketActivity basketActivity = new BasketActivity();
+    private List<Item> itemList = basketActivity.getBasketItem();
+    private ListInformationAdapter listAdapter;
+    private List<InfoDetail> info = new ArrayList<>();
+    private Item item;
+    Intent intentItem;
+
+    private ImageView image;
+    private TextView name;
+    private ImageView spicy;
+    private ImageView vegan;
+    private TextView price;
+    private LinearLayout isSales;
+    private TextView priceMain;
+    private TextView priceSales;
+    private TextView information;
+    private TextView minus;
+    private TextView howMany;
+    private TextView plus;
+    private Button addToCart;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_black_36px));
@@ -29,12 +60,89 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        Intent i = getIntent();
-        Item item = (Item) i.getSerializableExtra("item");
-        TextView textView = (TextView) findViewById(R.id.name_detail);
-        ImageView imageView = (ImageView) findViewById(R.id.image_id);
-        Picasso.with(getApplicationContext()).load(item.getImageUrl()).into(imageView);
-        textView.setText(item.getName());
+        name = findViewById(R.id.name_detail);
+        image = findViewById(R.id.image_id);
+        spicy = findViewById(R.id.spicy);
+        vegan = findViewById(R.id.vegan);
+        price = findViewById(R.id.price);
+        isSales = findViewById(R.id.sales);
+        priceMain = findViewById(R.id.priceMain);
+        priceSales = findViewById(R.id.salesPrice);
+        information = findViewById(R.id.info_item);
+        minus = findViewById(R.id.minus);
+        howMany = findViewById(R.id.howMany);
+        plus = findViewById(R.id.plus);
+        addToCart = findViewById(R.id.addToCart);
+        listView = findViewById(R.id.allInformation);
+
+        intentItem = getIntent();
+        item = (Item) intentItem.getSerializableExtra("item");
+        addInfoToList();
+        listAdapter = new ListInformationAdapter(getApplicationContext(), info);
+        listView.setAdapter(listAdapter);
+
+        name.setText(item.getName());
+        Picasso.with(getApplicationContext()).load(item.getImageUrl()).into(image);
+        spicy.setVisibility(item.isSpice() ? View.VISIBLE : View.GONE);
+        vegan.setVisibility(item.isVegetarian() ? View.VISIBLE : View.GONE);
+        price.setText(String.valueOf(item.getPrice() + " $"));
+        displaySales();
+        information.setText(item.getComment());
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               howMany.setText(String.valueOf(Integer.parseInt((String) howMany.getText()) - 1));
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                howMany.setText(String.valueOf(Integer.parseInt((String) howMany.getText()) + 1));
+            }
+        });
+      addToCart.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              if(howManyToInt() == 0) {
+                  itemList.add(item);
+                  basketActivity.setBasketItem(itemList);
+              } else {
+                  for (int i = 0; i < howManyToInt(); i++) {
+                      itemList.add(item);
+                      basketActivity.setBasketItem(itemList);
+                  }
+              }
+          }
+      });
+    }
+
+    private void displaySales() {
+        if (item.isSales()) {
+            isSales.setVisibility(View.VISIBLE);
+            priceMain.setText(item.getPrice() + " $");
+            priceMain.setPaintFlags(priceMain.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            priceSales.setText(item.getDiscontPrice() + " $");
+            // hide price TextView
+            price.setVisibility(View.GONE);
+        } else {
+            price.setVisibility(View.VISIBLE);
+            price.setText(item.getPrice() + " $");
+            // hide sales LinerLayout
+            isSales.setVisibility(View.GONE);
+        }
+    }
+
+    private Integer howManyToInt() {
+        return Integer.parseInt(String.valueOf(howMany.getText()));
+    }
+
+    private void addInfoToList() {
+        info.add(new InfoDetail("Gramms", String.valueOf(item.getGramms())));
+        info.add(new InfoDetail("Kalories", String.valueOf(item.getKalories())));
+        info.add(new InfoDetail("Protein", String.valueOf(item.getProtein())));
+        info.add(new InfoDetail("Carb", String.valueOf(item.getCarbohydrates())));
+        info.add(new InfoDetail("Status", item.getStatus()));
+        info.add(new InfoDetail("ComeFrom", item.getComeFrom()));
     }
 
 }
--- app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/ForgotPasswordActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/ForgotPasswordActivity.java
@@ -1,19 +1,56 @@
 package com.sanke46.android.e_commerce.ui.navigation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.sanke46.android.e_commerce.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private static final String TAG = "ForgotPasswordActivity";
+    private ProgressBar loddingSend;
+    private EditText forgotEmail;
+    private Button sendToEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Recover accaunt");
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Recover accaunt");
+
+        loddingSend = findViewById(R.id.loadingSend);
+        forgotEmail = findViewById(R.id.forgotEmail);
+        sendToEmail = findViewById(R.id.sendEmail);
+
+        sendToEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(String.valueOf(forgotEmail.getText()))
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Email sent.");
+                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                    Toast.makeText(getApplicationContext(), "New password send to your email", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
