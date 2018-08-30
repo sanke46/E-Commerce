6fcf0563599ba36c223c98eaa246eef9673e9627
sanke46 <shift.of.razum@gmail.com>
2018-08-30 21:17:59 +0300

    Add logo "Forgot_password"

--- app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/ForgotPasswordActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/ForgotPasswordActivity.java
@@ -5,16 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.sanke46.android.e_commerce.R;
+import com.sanke46.android.e_commerce.Utility.Helper;
 import com.sanke46.android.e_commerce.ViewModel.ForgotPasswordViewModel;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private ForgotPasswordViewModel forgotViewModel = new ForgotPasswordViewModel();
+    private Helper helper;
+
     private ProgressBar loddingProgressBar;
+    private ImageView logo;
     private EditText forgotEmailEditText;
     private Button buttonSend;
     private LinearLayout linearLayout;
@@ -24,7 +29,10 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        helper = new Helper(this);
+
         linearLayout = findViewById(R.id.liner);
+        logo = findViewById(R.id.logoForgotActivity);
         loddingProgressBar = findViewById(R.id.loadingProgressBar);
         forgotEmailEditText = findViewById(R.id.forgotEmail);
         buttonSend = findViewById(R.id.sendEmail);
@@ -32,7 +40,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loddingProgressBar.setVisibility(View.VISIBLE);
+                helper.doneLoadingActivity(loddingProgressBar, logo);
                 forgotViewModel.sendPasswordToEmail(getApplicationContext(),forgotEmailEditText);
             }
         });
