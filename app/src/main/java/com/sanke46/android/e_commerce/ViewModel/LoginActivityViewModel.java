541f3fe8f522b8f39626f4d4408dde53f2e79868
sanke46 <shift.of.razum@gmail.com>
2018-08-30 20:08:36 +0300

    LoginActivity + LoginViewModel

--- app/src/main/java/com/sanke46/android/e_commerce/ViewModel/LoginActivityViewModel.java
+++ app/src/main/java/com/sanke46/android/e_commerce/ViewModel/LoginActivityViewModel.java
@@ -1,4 +1,69 @@
 package com.sanke46.android.e_commerce.ViewModel;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.sanke46.android.e_commerce.MainActivity;
+import com.sanke46.android.e_commerce.Utility.Helper;
+import com.sanke46.android.e_commerce.ui.navigation.ForgotPasswordActivity;
+import com.sanke46.android.e_commerce.ui.navigation.LoginActivity;
+import com.sanke46.android.e_commerce.ui.navigation.SignUpActivity;
+
 public class LoginActivityViewModel {
+
+    private LoginActivity loginActivity;
+    private Helper helper;
+    private FirebaseAuth mAuth;
+
+    public LoginActivityViewModel(LoginActivity loginActivity) {
+        this.loginActivity = loginActivity;
+        helper = new Helper(loginActivity.getApplicationContext());
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void singIn(String email, String password, View login_progress, View logo) {
+        helper.doneLoadingActivity(login_progress,logo);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(loginActivity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LogingActivity", "signInWithEmail:success");
+                            loginActivity.startActivity(new Intent(loginActivity.getApplicationContext(), MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            logo.setVisibility(View.VISIBLE);
+                            login_progress.setVisibility(View.GONE);
+                            Log.w("LogingActivity", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(loginActivity.getApplicationContext(), "Authentication failed.",  Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void createNewAccaunt(){
+        loginActivity.startActivity(new Intent(loginActivity.getApplicationContext(), SignUpActivity.class));
+    }
+
+    public void fogotPassword(){
+        loginActivity.startActivity( new Intent(loginActivity.getApplicationContext(), ForgotPasswordActivity.class));
+    }
+
+    public void isArleadyLoginIn() {
+        // Test of login user or not
+        if (mAuth.getCurrentUser() != null) {
+            loginActivity.finish();
+            Log.v("LoginActivity", "** USER arleady login **");
+            loginActivity.startActivity(new Intent(loginActivity.getApplicationContext(), MainActivity.class));
+        }
+    }
 }
--- app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/LoginActivity.java
+++ app/src/main/java/com/sanke46/android/e_commerce/ui/navigation/LoginActivity.java
@@ -23,22 +23,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.sanke46.android.e_commerce.MainActivity;
 import com.sanke46.android.e_commerce.R;
+import com.sanke46.android.e_commerce.ViewModel.LoginActivityViewModel;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private LoginActivityViewModel loginViewModel;
     public FirebaseAuth.AuthStateListener mAuthListener;
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
 
     // UI references.
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
-    private View mLoginFormView;
     private TextView needAcc;
     private TextView fogotPass;
     private Button singInButton;
@@ -49,22 +46,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+
+        loginViewModel = new LoginActivityViewModel(this);
 
         // Set up the login form.
         logo = findViewById(R.id.logoLogIn);
         mEmailEditText = findViewById(R.id.email);
         mPasswordEditText = findViewById(R.id.password);
-        singInButton = findViewById(R.id.sign_in_button);
-        mLoginFormView = findViewById(R.id.login_form);
         login_progress = findViewById(R.id.login_progress);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animate);
+        logo.startAnimation(animation);
+
+        loginViewModel.isArleadyLoginIn();
 
         //Click links
         needAcc = findViewById(R.id.needAccaunt);
         needAcc.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                createNewAccaunt();
+                loginViewModel.createNewAccaunt();
             }
         });
 
@@ -72,74 +73,17 @@ public class LoginActivity extends AppCompatActivity {
         fogotPass.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                fogotPassword();
+                loginViewModel.fogotPassword();
             }
         });
 
-
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animate);
-        logo.startAnimation(animation);
-
-
-        // Test of login user or not
-        if (mAuth.getCurrentUser() != null) {
-            finish();
-            Log.v("LoginActivity","** USER arleady login **");
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        }
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d("LoginActivity", "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d("LoginActivity", "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
+        singInButton = findViewById(R.id.sign_in_button);
         singInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                singIn(mEmailEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim());
+                loginViewModel.singIn(mEmailEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim(),login_progress,logo);
             }
         });
     }
-
-    public void singIn(String email, String password) {
-        logo.setVisibility(View.GONE);
-        login_progress.setVisibility(View.VISIBLE);
-        mAuth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("LogingActivity", "signInWithEmail:success");
-                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                } else {
-                    // If sign in fails, display a message to the user.
-                    logo.setVisibility(View.VISIBLE);
-                    login_progress.setVisibility(View.GONE);
-                    Log.w("LogingActivity", "signInWithEmail:failure", task.getException());
-                    Toast.makeText(getApplicationContext(), "Authentication failed.",  Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public void createNewAccaunt(){
-        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
-    }
-
-    public void fogotPassword(){
-        startActivity( new Intent(getApplicationContext(), ForgotPasswordActivity.class));
-    }
-
 }
 
